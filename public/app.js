2cb8a6e0fd7e9bca804e93724ff6d3e7fc7b317d
Tane4ka170 <tane4ka170@gmail.com>
2024-09-28 23:16:21 +0300

    User msgs, typing indicator added

--- public/app.js
+++ public/app.js
@@ -35,11 +35,56 @@ document.querySelector(".form-join").addEventListener("submit", enterRoom);
 
 socket.on("message", (data) => {
   activity.textContent = "";
+  const { name, text, time } = data;
   const li = document.createElement("li");
-  li.textContent = data;
-  document.querySelector("ul").appendChild("li");
+  li.className = "post";
+
+  if (name === nameInput.value) li.className = "post post--left";
+  if (name !== nameInput.value && name !== "Admin")
+    li.className = "post post--right";
+  if (name !== "Admin") {
+    li.innerHTML = `<div class="post__header ${
+      name === nameInput.value ? "post__header--user" : "post__header--reply"
+    }">
+        <span class="post__header--name">${name}</span> 
+        <span class="post__header--time">${time}</span> 
+        </div>
+        <div class="post__text">${text}</div>`;
+  } else {
+    li.innerHTML = `<div class="post__text">${text}</div>`;
+  }
+  document.querySelector(".chat-display").appendChild("li");
+  chatDisplay.scrollHeight = chatDisplay.scrollTop;
 });
 
 msgInput.addEventListener("keypress", () => {
   socket.emit("activity", nameInput.value);
 });
+
+let activityTimer;
+socket.on("activity", (name) => {
+  activity.textContent = `${name} is typing...`;
+
+  // Clear after 3 seconds
+  clearTimeout(activityTimer);
+  activityTimer = setTimeout(() => {
+    activity.textContent = "";
+  }, 3000);
+});
+
+function showUsers(users) {
+  usersList.textContent = "";
+
+  if (users) {
+    usersList.textContent = "";
+    if (users) {
+      usersList.innerHTML = `<em>Users in ${chatRoom.value}:</em>`;
+      users.forEach((user, i) => {
+        usersList.textContent += ` ${user.name}`;
+        if (users.length > 1 && i !== users.length - 1) {
+          usersList.textContent += ",";
+        }
+      });
+    }
+  }
+}
